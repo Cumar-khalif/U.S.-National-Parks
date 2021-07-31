313070ab2e005d16be0e94c1709d88096940c3b1
David Tierney <daveshouse44@hotmail.com>
2021-07-31 11:43:23 -0500

    added js

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,10 @@
-fetch("https://developer.nps.gov/api/v1/parks?stateCode=mn&api_key=bXu3Ai3Odu0e1HKfSDrMMWwCGmh9e2AEvwa80Dx6")
+$(".btn").on("click", function (event) {
+    event.preventDefault();
+
+    var stateSel = $("state").value
+    var getApiUrl = "https://developer.nps.gov/api/v1/parks?stateCode=" + stateSel + "&api_key=bXu3Ai3Odu0e1HKfSDrMMWwCGmh9e2AEvwa80Dx6";
+    
+    fetch (getApiUrl)
 .then(response => response.json())
 .then (data => console.log(data));
-
+})
