176b450502eb9bde9fcced732f052978dec34f03
Jose Avalos <avaljose@hotmai.com>
2021-07-31 15:29:01 -0500

    my changes

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,19 @@
-$(".btn").on("click", function (event) {
-    event.preventDefault();
 
-    var stateSel = $("state").value
-    var getApiUrl = "https://developer.nps.gov/api/v1/parks?stateCode=" + stateSel + "&api_key=bXu3Ai3Odu0e1HKfSDrMMWwCGmh9e2AEvwa80Dx6";
+    var parkContainerEl = $("#park-container") 
+
+$("#submit").on("click", function (event) {
+    event.preventDefault();
+    getParks();
     
+    
+})
+function getParks(){
+    
+    var stateSel = $("#state").val();
+    var getApiUrl = "https://developer.nps.gov/api/v1/parks?stateCode=" + stateSel + "&api_key=bXu3Ai3Odu0e1HKfSDrMMWwCGmh9e2AEvwa80Dx6";
+
+    console.log(stateSel)
     fetch (getApiUrl)
 .then(response => response.json())
 .then (data => console.log(data));
-})
+}
